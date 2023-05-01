ed000a73119f0b2928b8e3f534a5ce7dfc466a7c
acreturs <acornett@gmx.de>
2023-05-01 16:19:22 +0200

    final changes

--- app.js
+++ app.js
@@ -1,54 +1,51 @@
-require('dotenv').config();
+require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// Define the function to be called when the button is clicked
-async function  myServerFunction() {
-    console.log("hintere Methode erreicht")
-    // console.log(process.env.OPENAI_API_KEY)
-    const configuration = new Configuration({
-         apiKey:process.env.OPENAI_API_KEY, //process.env.OPENAI_API_KEY,
-       });
-    console.log("erstellt")
-    return configuration;
+async function createConfiguration() {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  console.log("Configuration erstellt");
+  return configuration;
 }
 
+/*
+Requests the Answer from Chatgtp but to use await to avoid using variables without declaring them first we use an async function createConfiguration()
+*/
+async function openAIRequest() {
+  createConfiguration();
+  const res = await createConfiguration();
+  const openai = new OpenAIApi(res);
+  const completion = openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: "Explain Harry Potter to me like I am 5 in 150 words",
+    max_tokens: 200,
+    n: 1,
+  });
 
-async function warten(){
-    myServerFunction()
-    console.log("warten erreicht")
-    const res = await myServerFunction()
-    const openai = new OpenAIApi(res);
-    console.log("was soll das");
-    const completion = openai.createCompletion({
-     model: "text-davinci-002",
-     prompt: "Explain Harry Potter to me like I am 5 in 150 words",
-     max_tokens:200,
-     n:1
-   });
-
-
-   const com = await completion
-   //console.log((await completion).data)
-
-console.log(com.data.choices[0].text);
-   
-   
-   console.log(com.data)
+  const com = await completion; //hier noch mit Infos rumspelen
+  console.log(com.data.choices[0].text);
 }
 
-// Add a route to the server to handle the HTTP request from the button click
-app.get('/myserverfunction', warten);
-app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html');
-})
+/*
+The get request via express.js towards localhost/requestURL. This routing is 
+currently just used for organizational purposes.
+*/
+app.get("/requestURL", openAIRequest);
 
+/*
+import the HTML file to send to the client 
+*/
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-
-
-// Start the server
+/*
+Localhost is currently running on port 3000
+*/
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Läuft auf 3k");
 });
